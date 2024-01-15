b855fa2f88ccab210adc24c699b75541e179eb0f
TANZEELA S <muc.555@gmail.com>
2024-01-15 12:20:52 +0500

    card component : week special

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Card(props){
+    return(
+        <div className="card">
+            <div className="card-img">
+                <img src={props.img} alt={props.alt}></img>
+            </div>
+            <div className="card-title">
+                <h3>{props.title}</h3>
+            </div>
+            <div className="card-price">
+                <h3>{props.price}</h3>
+            </div>
+            <div className="card-about">
+                <p>{props.about}</p>
+            </div>
+            <a href={props.link}>Order a delivery</a>
+        </div>
+    )
+}
+
+export default Card;
