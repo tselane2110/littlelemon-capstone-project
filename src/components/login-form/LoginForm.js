90c4976aff7a22df9d5291b871b2c94db380b82b
TANZEELA S <muc.555@gmail.com>
2024-01-29 15:23:05 +0500

    initialize LoginForm Page

---
+++ src/components/login-form/LoginForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LoginForm.css";
+
+
+function LoginForm(){
+    return(
+        <div className="form">
+            <div className="form-size">
+
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm;
