5ac2e9f3857abdf15e91bff856069e27c0645fe5
TANZEELA S <muc.555@gmail.com>
2024-01-29 22:22:52 +0500

    add: copyright message

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,47 +2,63 @@ import React from "react";
 import icon from "../../icons_assets/Logo .svg";
 import "./footer.css";
 
+// get current date
+
+const getCurrentDate = () => {
+    const today = new Date();
+    //const day = today.getDate().toString().padStart(2, '0');
+    //const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
+    const year = today.getFullYear();
+
+    return `${year}`;
+
+    // return `${day}-${month}-${year}`;
+}
+
 function Footer(){
     return(
-        <footer className="footer">
-        <div className="footer-size">
-        <div className="footer-logo">
-            <img src={icon} alt='icon'/>
-        </div>
-        <div className="col col1">
-            <h5>
-                Doormat Navigation
-            </h5>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Login</li>
-            </ul>
-        </div>
-        <div className="col col2">
-            <h5>
-                Contact US
-            </h5>
-            <ul>
-                <li>Email</li>
-                <li>Address</li>
-                <li>Phone Number</li>
-            </ul>
-        </div>
-        <div className="col col3">
-            <h5>
-                Social Media Links
-            </h5>
-            <ul>
-                <li>Facebook</li>
-                <li>Instagram</li>
-            </ul>
-        </div>
-        </div>
-        </footer>
+        <>
+            <footer className="footer">
+            <div className="footer-size">
+            <div className="footer-logo">
+                <img src={icon} alt='icon'/>
+            </div>
+            <div className="col col1">
+                <h5>
+                    Doormat Navigation
+                </h5>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+            <div className="col col2">
+                <h5>
+                    Contact US
+                </h5>
+                <ul>
+                    <li>Email</li>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                </ul>
+            </div>
+            <div className="col col3">
+                <h5>
+                    Social Media Links
+                </h5>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                </ul>
+            </div>
+            </div>
+            </footer>
+            <p className="copyright-msg">Copyright © {getCurrentDate()} by <a href="https://github.com/tselane2110" target="_blank">Tanzeela Sehar</a> All Rights Reserved.</p>
+        </>
     )
 }
 
