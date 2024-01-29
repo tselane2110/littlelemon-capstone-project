c728d4639f722fe1a053d2b05ca9ead848f3b6d1
TANZEELA S <muc.555@gmail.com>
2024-01-29 15:03:20 +0500

    add: function - scrollToSection

--- src/components/nav/Menu-Nav.js
+++ src/components/nav/Menu-Nav.js
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom";
 import "./Menu-Nav.css"
 
 function MenuNav(props){
+    const menu = props.menu;
+    const scrollToSection = (sectionId) => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+          section.scrollIntoView({ behavior: 'smooth' });
+        }
+      };
     return(
         <div className="menu-nav">
             <div className="menu-nav-size">
-                <Link to={props.menu[0]} className="menu-item-link">Appetizers</Link>
-                <Link to={props.menu[1]} className="menu-item-link">Salads</Link>
-                <Link to={props.menu[2]} className="menu-item-link">Main Course</Link>
-                <Link to={props.menu[3]} className="menu-item-link">Sides</Link>
-                <Link to={props.menu[4]} className="menu-item-link">Deserts</Link>
-                <Link to={props.menu[5]} className="menu-item-link">Beverages</Link>
+                <Link to="/orderOnline#appetizers" className="menu-item-link" onClick={() => scrollToSection(menu[0])}>Appetizers</Link>
+                <Link to="/orderOnline#salads" className="menu-item-link" onClick={() => scrollToSection(menu[1])}>Salads</Link>
+                <Link to="/orderOnline#main-courses" className="menu-item-link" onClick={() => scrollToSection(menu[2])}>Main Course</Link>
+                <Link to="/orderOnline#sides" className="menu-item-link" onClick={() => scrollToSection(menu[3])}>Sides</Link>
+                <Link to="/orderOnline#deserts" className="menu-item-link" onClick={() => scrollToSection(menu[4])}>Deserts</Link>
+                <Link to="/orderOnline#beverages" className="menu-item-link" onClick={() => scrollToSection(menu[5])}>Beverages</Link>
             </div>
         </div>
     )
